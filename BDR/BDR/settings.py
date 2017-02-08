fd2de0d10ad655e533d274b83db65d8d883d4ee1
Rémi <remibouvet72@gmail.com>
2017-02-08 10:22:46 +0100

    debut template

--- BDR/BDR/settings.py
+++ BDR/BDR/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'BDR.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['core/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
