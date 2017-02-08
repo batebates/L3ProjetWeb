f3a9904c8444b6f64749d005f62bded26b170341
Rémi <remibouvet72@gmail.com>
2017-02-08 09:42:16 +0100

    Ajout de la vue

--- BDR/BDR/urls.py
+++ BDR/BDR/urls.py
@@ -17,8 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'^user/', include('user.urls')),
-	url(r'^salon/', include('salon.urls')),
 	url(r'^', include('core.urls')),
     url(r'^admin/', admin.site.urls),
 ]
