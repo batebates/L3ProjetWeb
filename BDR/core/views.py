d4688215efd00bcb60549b4b9996573a48f409a9
Rémi <remibouvet72@gmail.com>
2017-02-08 09:31:04 +0100

    suppression des apps

--- BDR/core/views.py
+++ BDR/core/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-def index(request):
-	return HttpResponse("Hello, world. You're at the core index ")
+# Create your views here.
