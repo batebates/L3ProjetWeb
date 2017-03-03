ce7e42398e733ef5bb1f3e43d6a99c86afa569eb
dimitri bernot <spi3084@mordor.univ-lemans.fr>
2017-03-03 11:35:48 +0100

    Création de salles

--- BDR/room/views.py
+++ BDR/room/views.py
@@ -1,17 +1,42 @@
-from django.http import HttpResponse
-from django.http import Http404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.template import RequestContext
+from django.views.decorators.csrf import csrf_protect
+from django.urls import reverse
 
 from room.models import Room
 
 def index(request):
-	context = ""
-	return render(request, 'room/index.html', context)
+	open_rooms_list = Room.objects.filter(roomIsOpen=True)
+	
+	answer = ""
+	for e in open_rooms_list:
+		answer += "<a href='/room/" + str(e.id) + "'>" + str(e.id) + "</a> <br />"
+	
+	return HttpResponse(answer)
+	
+	#context = ""
+	#return render(request, 'room/index.html', context)
 
 def detail(request, idRoom):
 	try:
 		room = Room.objects.get(pk=idRoom)
-		return HttpResponse("Welcome to room %s !" % idRoom)
+		return render(request, 'room/roomOpen.html', {'room':room})
 	except Room.DoesNotExist:
-		return HttpResponse("Room does not exist yet")
+		context = ""
+		return render(request, 'room/noRoom.html', {'idRoom':idRoom})
+
+
+@csrf_protect
+def creation(request, idRoom):
+	csrfContext = RequestContext(request, idRoom)
+	try:
+		room = Room.objects.get(pk=idRoom)
+		return render(request, 'room/roomOpen.html', csrfContext, {'room':room})
+	except Room.DoesNotExist:
+		room = Room()
+		room.pk=idRoom
+		room.roomIsOpen=True
+		room.save()
+		return HttpResponseRedirect(reverse('room:detail', args=[idRoom]))
