d93bf5e4be02e40d2b2dd3e7caea5620396646be
batebates <root@batebatesPortable>
2017-03-07 20:41:02 +0100

    gestion des comptes()

--- BDR/core/urls.py
+++ BDR/core/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^connexion$', views.connexion, name='connexion'),
+    url(r'^deconnexion$', views.deconnexion, name='deconnexion'),
+    url(r'^inscription$', views.inscription, name='inscription'),
     url(r'^$', views.index, name='index'),
 ]
--- BDR/core/views.py
+++ BDR/core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Template
-
+from .forms import ConnexionForm,InscriptionForm
 from room.models import Room
 
 def index(request):
@@ -9,3 +9,52 @@ def index(request):
 	first_closed_room = open_rooms_list.reverse().first()
 	first_closed_room_id = first_closed_room.id + 1
 	return render(request, 'core/index.html', {'openRooms' : open_rooms_list, 'closedRoomId' : first_closed_room_id})
+
+from django.contrib.auth import authenticate, login
+
+def connexion(request):
+    error = False
+
+    if request.method == "POST":
+        form = request.POST
+        print form
+        if form:
+            username = form["pseudo"]
+            password = form["password"]
+            user = authenticate(username=username, password=password)  # Nous verifions si les donnees sont correctes
+            if user:  # Si l'objet renvoye n'est pas None
+                login(request, user)  # nous connectons l'utilisateur
+            else: # sinon une erreur sera affichee
+                error = True
+
+    return render(request, 'core/index.html')
+
+from django.contrib.auth import logout
+from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.shortcuts import redirect
+
+def deconnexion(request):
+    logout(request)
+    return redirect(reverse(connexion))
+
+from django.contrib.auth.models import User
+
+def inscription(request):
+    error = False
+    if request.method == "POST":
+        form = request.POST
+        print form
+        if form:
+            username = form["pseudo"]
+            password = form["password"]
+            email = form["email"]
+            user = User.objects.create_user(username,email, password)
+            if user:  # Si l'objet renvoye n'est pas None
+                login(request, user)  # nous connectons l'utilisateur
+            else: # sinon une erreur sera affichee
+                error = True
+    else:
+        form = ConnexionForm()
+
+    return render(request, 'core/index.html')
