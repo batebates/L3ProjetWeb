c592e3b7890096cc3ee8eef38cf979b53225974b
dimitri bernot <spi3084@ic2s114-15.univ-lemans.fr>
2017-03-07 16:28:40 +0100

    Possibilité de voir les salles ouvertes sur l'index

--- BDR/core/views.py
+++ BDR/core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import Template
 
+from room.models import Room
 
 def index(request):
-    return render(request, 'core/index.html', {})
+	open_rooms_list = Room.objects.filter(roomIsOpen=True)
+	return render(request, 'core/index.html', {'rooms' : open_rooms_list})
