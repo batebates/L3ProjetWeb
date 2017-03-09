adb8274469c91116d5f72de6439fec014741d559
batebates <root@batebatesPortable>
2017-03-09 13:00:38 +0100

    correction(2) views

--- BDR/core/views.py
+++ BDR/core/views.py
@@ -8,7 +8,7 @@ import re
 
 def validateEmail(email):
     if len(email) > 6:
-        if re.match('\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b', email) != None:
+        if re.match('\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b' , email) != None:
             return 1
     return 0
 
